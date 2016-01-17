772626e1b4a6562dd16ff174df90f35b1d372a30
caijie <joseph7011@163.com>
2016-01-18 04:24:23 +0800

    Initial Commit

---
+++ sina/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sina
+//
+//  Created by Joseph on 16/1/18.
+//  Copyright © 2016年 运来. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ sina/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  sina
+//
+//  Created by Joseph on 16/1/18.
+//  Copyright © 2016年 运来. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
